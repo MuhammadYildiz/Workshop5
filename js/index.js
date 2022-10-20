52208a853ee698999a124810590a3de979b2d7eb
Muhammad Yizimu <64603940+MuhammadYildiz@users.noreply.github.com>
2022-10-20 14:05:14 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+ let summa = document.getElementById('sum');
+ let personer = document.getElementById('numberOfFriends');
+ let procent = document.getElementById('tip');
+ let result = document.getElementById('result');
+function calculate(){
+    result.innerHTML =  Number(summa.value) / Number(personer.value ) + (Number(procent.value)/100) + ' KR';  ;
+}
